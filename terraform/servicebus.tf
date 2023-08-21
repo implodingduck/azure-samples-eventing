2a1fdea252a60788609385afda1b400a0c6befe8
implodingduck <zsek320@gmail.com>
2023-08-20 19:20:06 -0500

    adding service bus topic filters

--- terraform/servicebus.tf
+++ terraform/servicebus.tf
@@ -25,3 +25,17 @@ resource "azurerm_servicebus_subscription" "multiply" {
   topic_id           = azurerm_servicebus_topic.this.id
   max_delivery_count = 1
 }
+
+resource "azurerm_servicebus_subscription_rule" "sum" {
+  name            = "rule-${local.func_name}-sum"
+  subscription_id = azurerm_servicebus_subscription.sum.id
+  filter_type     = "SqlFilter"
+  sql_filter      = "operation = 'sum'"
+}
+
+resource "azurerm_servicebus_subscription_rule" "multiply" {
+  name            = "rule-${local.func_name}-multiply"
+  subscription_id = azurerm_servicebus_subscription.sum.id
+  filter_type     = "SqlFilter"
+  sql_filter      = "operation = 'multiply'"
+}

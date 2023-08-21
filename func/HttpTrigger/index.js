387a21e108dd46f34ed4f83b9791f49203032d9d
implodingduck <zsek320@gmail.com>
2023-08-21 11:01:41 -0500

    adjusting http trigger for testing

--- func/HttpTrigger/index.js
+++ func/HttpTrigger/index.js
@@ -1,3 +1,30 @@
+const { BlobServiceClient } = require('@azure/storage-blob');
+const { DefaultAzureCredential } = require('@azure/identity');
+require('dotenv').config()
+
+const accountName = process.env.SANAME;
+const blobServiceClient = new BlobServiceClient(
+  `https://${accountName}.blob.core.windows.net`,
+  new DefaultAzureCredential()
+);
+
+async function streamToBuffer(readableStream, context) {
+    context.log("lets streamToBuffer")
+    return new Promise((resolve, reject) => {
+        const chunks = [];
+        readableStream.on("data", (data) => {
+            context.log("onData")
+            chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+        });
+        readableStream.on("end", () => {
+            context.log("onEnd")
+            resolve(Buffer.concat(chunks));
+        });
+        readableStream.on("error", reject);
+    });
+}
+
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
@@ -15,8 +42,21 @@ module.exports = async function (context, req) {
     ]
 
     context.bindings.outputSbTopic = message;
+    
+    const blobName = "sample.csv"
+    context.log(`Downloading ${blobName}`);
+    const containerClient = await blobServiceClient.getContainerClient("upload");
+    const blobClient = containerClient.getBlobClient(blobName);
+    const downloadResponse = await blobClient.download();
+    context.log(downloadResponse)
+    const downloaded = await streamToBuffer(downloadResponse.readableStreamBody, context);
+    context.log("Downloaded blob content:", downloaded.toString());
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: message
+        body: {
+                "servicebusmessage": message,
+                "blobcontent": downloaded.toString()
+        }
     };
 }

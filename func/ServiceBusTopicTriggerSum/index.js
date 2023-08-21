f903bb63110ee201ff7069d1ce89aef9ae016881
implodingduck <zsek320@gmail.com>
2023-08-20 19:09:26 -0500

    making the sb triggers do something

--- func/ServiceBusTopicTriggerSum/index.js
+++ func/ServiceBusTopicTriggerSum/index.js
@@ -1,3 +1,4 @@
 module.exports = async function(context, mySbMsg) {
     context.log('JavaScript ServiceBus topic trigger function processed message', mySbMsg);
+    context.log(`${mySbMsg.v1} + ${mySbMsg.v2} = ${mySbMsg.v1 + mySbMsg.v2}`) 
 };
